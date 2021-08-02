d49a4a02be7fcd698f535b1982e0ba6e86692883
Gabriel Peralta <prates_peralta@hotmail.com>
2021-08-02 11:40:02 -0300

    adiciona mais exercícios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ARTHUR.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ARTHUR.js
@@ -1,29 +1,6 @@
-let resultado = "";
-let asterisco = "*"
+let array = ["*", "*", "*", "*", "*"]
 
-
-for(let i = 0; i < 1; i += 1) {
-  resultado += asterisco[i]
-  console.log(resultado);
+for(let i = 0; i < array.length; i += 1) {
+let resultado = array[i] + "";
+console.log(resultado);
 }
-console.log("");
-for(let j = 0; j < 2; j += 1) {
-  resultado += asterisco[j]
-  console.log(resultado);
-}
-console.log("");
-for(let l = 0; l < 3; l += 1) {
-  resultado += asterisco[l]
-  console.log(resultado);
-}
-console.log("");
-for(let m = 0; m < 4; m += 1) {
-  resultado += asterisco[m]
-  console.log(resultado);
-}
-console.log("");
-for(let n = 0; n < 5; n += 1) {
-  resultado += asterisco[n]
-  console.log(resultado);
-}
-
