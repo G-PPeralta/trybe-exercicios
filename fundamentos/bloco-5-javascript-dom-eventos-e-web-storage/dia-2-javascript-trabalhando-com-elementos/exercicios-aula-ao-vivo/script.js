84f82a10a6c1964bbdbdcf2f16ec95be37f256f6
Gabriel Peralta <prates_peralta@hotmail.com>
2021-08-10 13:49:35 -0300

    adiciona o exercicio 1 da aula ao vivo

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios-aula-ao-vivo/script.js
@@ -0,0 +1,7 @@
+//Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+
+let title = document.createElement("h1");
+title.innerText = "Exercício 5.2 - JavaScript DOM";
+document.body.appendChild(title);
+
+//Adicione a tag main com a classe main-content como filho da tag body ;
