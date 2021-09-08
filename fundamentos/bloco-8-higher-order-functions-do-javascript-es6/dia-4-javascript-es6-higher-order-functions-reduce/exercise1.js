17500ad16ce03e2c4ea0e237a235cac440604b26
Gabriel Peralta <prates_peralta@hotmail.com>
2021-09-08 10:57:37 -0300

    adiciona o primeiro exercicio

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((a, b) => a.concat(b), []);
+}
+
+/*Solução alternativa sem o reduce:
+function flatten() {
+  return arrays.flat();
+}*/
+
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
