292408dd47b17f04793edf48dbc0766bc26c3480
Gabriel Peralta <prates_peralta@hotmail.com>
2021-10-09 09:17:37 -0300

    adiciona o requisito 1

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/my-app/src/Components/Pokemon.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+class Pokemon extends React.Component {
+  render() {
+
+    const {pokemon} = this.props;
+
+    const {name, type, averageWeight, image} = pokemon;
+
+    return (
+      <div style={container}>
+        <div>
+          <p>{name}</p>
+          <p>{type}</p>
+          <p>{averageWeight.value} {averageWeight.measurementUnit}</p>
+        </div>
+        <img src={image} alt={name}></img>
+      </div>
+    )
+  }
+}
+
+const container = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  border: '1px solid grey',
+  borderRadius: '10px',
+  alignItems: 'center',
+  justifyContent: 'space-around',
+  flex: '1 1 0',
+  padding: '10px',
+  minWidth: '25%',
+}
+
+Pokemon.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      measurementUnit: PropTypes.string,
+      value: PropTypes.number,
+    }),
+    image: PropTypes.string,
+  }).isRequired,
+};
+
+export default Pokemon
