273b4b56c41431a18516c198b84427d17c9f4dc6
Gabriel Peralta <prates_peralta@hotmail.com>
2022-02-21 15:37:45 -0300

    exercicios de REST

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicios/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const products = require('./controllers/productController');
+
+const app = express();
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/products', products);
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+});
